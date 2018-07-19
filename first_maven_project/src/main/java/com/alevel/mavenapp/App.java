82b241b0653b8b8133e9a27a8a4e1999499d3d52
Leowario <voronventusa@gmail.com>
2018-07-19 19:40:42 +0300

    Added

--- first_maven_project/src/main/java/com/alevel/mavenapp/App.java
+++ first_maven_project/src/main/java/com/alevel/mavenapp/App.java
@@ -15,6 +15,34 @@ public class App {
         handler.addServlet(FirstServlet.class, "/");
 
         server.start();
+
+//        String string = "6,5,7,8,9,1,2,3";
+//
+//        String arr[] = string.split(",");
+//        String sorted[]= sort(arr);
+//
+//        for (int i = 0; i < sorted.length; i++) {
+//
+//            System.out.println(sorted[i]);
+//        }
+    }
+
+    static String[] sort(String[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                int first = Integer.parseInt(arr[i]);
+                int second = Integer.parseInt(arr[j]);
+                if (first > second) {
+                    int temp = first;
+                    arr[i] = second + "";
+                    arr[j] = first + "";
+                    i++;
+                }
+            }
+        }
+        return arr;
     }
 }
 
