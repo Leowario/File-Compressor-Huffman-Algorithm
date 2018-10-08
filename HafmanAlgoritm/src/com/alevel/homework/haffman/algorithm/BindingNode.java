b10d043277d69b8e50a5670ae73bbb189095707d
Leowario <voronventusa@gmail.com>
2018-10-08 19:28:48 +0300

    Class BindingNode corrected.

--- HafmanAlgoritm/src/com/alevel/homework/haffman/algorithm/BindingNode.java
+++ HafmanAlgoritm/src/com/alevel/homework/haffman/algorithm/BindingNode.java
@@ -5,17 +5,17 @@ class BindingNode extends Node {
     Node left;
     Node right;
 
-    @Override
-    void buildCode(String code) {
-        super.buildCode(code);
-        left.buildCode(code + "0");
-        right.buildCode(code + "1");
-    }
-
     BindingNode(Node left, Node right) {
         super(left.sum + right.sum);
         this.left = left;
         this.right = right;
         sum = left.sum + right.sum;
+    }
+
+    @Override
+    void buildCode(String code) {
+        super.buildCode(code);
+        left.buildCode(code + "0");
+        right.buildCode(code + "1");
     }
 }
